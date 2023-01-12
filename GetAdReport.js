c3c60c93ec41e04233d6ed62ff540e38f88ae654
Oleg Piddubnyi <opiddubny@gmail.com>
2023-01-12 15:15:38 +0200

    Code Cleaning

--- GetAdReport.js
+++ GetAdReport.js
@@ -51,22 +51,19 @@ const CONFIG = {
         "languageSetup": `A2`
   }
 
+// F - Function creates dictionary (Obj) of current set up languages.
 function getLangSettings() {
-  
     const campaigns = AdsApp.campaigns()
         .withCondition("campaign.status = 'ENABLED'")
         .get();
     
     let langDictionary = {};
-
     while (campaigns.hasNext()){
         const campaign = campaigns.next();
         const campaign_name = campaign.getName();
         const campaign_ID = campaign.getId();
         const languages = campaign.targeting().languages().get();
-        
         let languageList = [];
-
         if (languages.hasNext()) {
             while (languages.hasNext()) {
                 const language = languages.next();
@@ -80,19 +77,15 @@ function getLangSettings() {
 
         langDictionary[campaign_ID] = languageList;
 
-
     }
     
     return langDictionary;
 }
   
-
+// F - This function transform adaptive ads types to unit with regular Ads form
 function transformAds (list, type) {
-    
     let obj = {};
-//    console.log(type);
     let quantity;
-
     if (type === "Headline") {
         quantity = 15;
     } else if (type === "Description") {
@@ -102,9 +95,7 @@ function transformAds (list, type) {
     }
     
     for (let row = 0; row < quantity; row++){
-      
       let rowCounter = type + " " + (parseInt(row) + 1);
-
       if (!!list[row]) {
         if (!!list[row]["text"]) {
             obj[rowCounter] = list[row]["text"]
@@ -115,31 +106,24 @@ function transformAds (list, type) {
         obj[rowCounter] = ""
       }
       
-      
     }
     
     return obj;
-  }
-  
-function transformReport(report, adsLanguage){
-    
-    const result = [];
+}
 
+// F - This function detects Ads types and creates the report structure required for Google Spreadsheets
+function transformReport(report, adsLanguage){
+    const result = [];
     const langDictionary = getLangSettings();
-  
     const rows = report.rows();
     while (rows.hasNext()) {
-  
         let row = rows.next();
-
-//        prettyPrint(row);
-
         let headlineRow = {};
         let descriptionRow = {};    
         let pathRow = {};
         let longHeadline = {};
 
-//        console.log(row["ad_group_ad.ad.type"]);
+        // Ads Types Detection
         if (row["ad_group_ad.ad.type"] === "RESPONSIVE_SEARCH_AD") {
             headlineRow = transformAds(row["ad_group_ad.ad.responsive_search_ad.headlines"], "Headline");
             descriptionRow = transformAds(row["ad_group_ad.ad.responsive_search_ad.descriptions"], "Description");    
@@ -236,6 +220,7 @@ function transformReport(report, adsLanguage){
             };
         }
 
+      // Creating report structure  
       result.push(
         {
           "Account ID": row["customer.id"],
@@ -257,9 +242,10 @@ function transformReport(report, adsLanguage){
     }
   
     return result;
-  }
+}
   
-  function exportReport(sheet, spreadsheet, reportTable) {
+// F - Exporting New Report into Google Spreadsheet
+function exportReport(sheet, spreadsheet, reportTable) {
     const report = reportTable;
   
     // Creating Headline for a report table, if it's empty
@@ -271,20 +257,19 @@ function transformReport(report, adsLanguage){
     let reportArray = [];
     for(const row of report){
         reportArray.push(Object.values(row));
-      }
+    }
     
     // Set an empty range in Google Spreadsheet for uploading data from Array
     let range = sheet.getRange(2, 1,  (reportArray.length), (reportArray[0].length));
     range.setValues(reportArray); 
     
-  }
+}
   
-  
-  function prettyPrint(obj){
+ /// F - Is needed for debugging some code 
+function prettyPrint(obj){
     console.log(JSON.stringify(obj, null, 4));
-  }
-  
-  
+}
+    
 function main() {
     const spreadsheet = SpreadsheetApp.openByUrl(CONFIG.sheetURL);
     let sheet = spreadsheet.getSheetByName(CONFIG.sheetName);
@@ -305,70 +290,60 @@ function main() {
 }
 
 /*
-+           customer.id, 
-+           campaign.id, 
-+           campaign.name, 
-+           ad_group.id, 
-+           ad_group.name, 
-+           ad_group_ad.ad.id, 
-+           ad_group_ad.ad.type, 
+***** Info Part *****
 
-DISCOVERY_MULTI_ASSET_AD ->
+Curent Script Version is working with next types of adds:
 
+EXPANDED_DYNAMIC_SEARCH_AD
+            ad_group_ad.ad.expanded_dynamic_search_ad.description, 
+            ad_group_ad.ad.expanded_dynamic_search_ad.description2, 
+
+EXPANDED_TEXT_AD
+            ad_group_ad.ad.expanded_text_ad.headline_part1, 
+            ad_group_ad.ad.expanded_text_ad.headline_part2, 
+            ad_group_ad.ad.expanded_text_ad.headline_part3, 
+            ad_group_ad.ad.expanded_text_ad.description, 
+            ad_group_ad.ad.expanded_text_ad.description2, 
+            ad_group_ad.ad.expanded_text_ad.path1, 
+            ad_group_ad.ad.expanded_text_ad.path2, 
+
+RESPONSIVE_DISPLAY_AD ->
+             ad_group_ad.ad.responsive_display_ad.headlines, 
+             ad_group_ad.ad.responsive_display_ad.long_headline, 
+             ad_group_ad.ad.responsive_display_ad.descriptions, 
+
+RESPONSIVE_SEARCH_AD
+            ad_group_ad.ad.responsive_search_ad.headlines, 
+            ad_group_ad.ad.responsive_search_ad.descriptions, 
+            ad_group_ad.ad.responsive_search_ad.path1, 
+            ad_group_ad.ad.responsive_search_ad.path2, 
+
+TEXT_AD
+             ad_group_ad.ad.text_ad.headline,
+            ad_group_ad.ad.text_ad.description2,
+            ad_group_ad.ad.text_ad.description1 
+
+***** ----- *****
+
+Upgoing types:
+
+DISCOVERY_MULTI_ASSET_AD
             ad_group_ad.ad.discovery_multi_asset_ad.headlines, 
             ad_group_ad.ad.discovery_multi_asset_ad.descriptions, 
 
-EXPANDED_DYNAMIC_SEARCH_AD ->
-
-+            ad_group_ad.ad.expanded_dynamic_search_ad.description, 
-+            ad_group_ad.ad.expanded_dynamic_search_ad.description2, 
-
-EXPANDED_TEXT_AD
-
-+            ad_group_ad.ad.expanded_text_ad.headline_part1, 
-+            ad_group_ad.ad.expanded_text_ad.headline_part2, 
-+            ad_group_ad.ad.expanded_text_ad.headline_part3, 
-+            ad_group_ad.ad.expanded_text_ad.description, 
-+            ad_group_ad.ad.expanded_text_ad.description2, 
-+           ad_group_ad.ad.expanded_text_ad.path1, 
-+           ad_group_ad.ad.expanded_text_ad.path2, 
-
 LEGACY_RESPONSIVE_DISPLAY_AD
-
             ad_group_ad.ad.legacy_responsive_display_ad.short_headline, 
             ad_group_ad.ad.legacy_responsive_display_ad.long_headline, 
             ad_group_ad.ad.legacy_responsive_display_ad.description, 
 
-LOCAL_AD ->
-
+LOCAL_AD
             ad_group_ad.ad.local_ad.headlines, 
             ad_group_ad.ad.local_ad.descriptions, 
             ad_group_ad.ad.local_ad.path1, 
             ad_group_ad.ad.local_ad.path2, 
 
-RESPONSIVE_DISPLAY_AD ->
-
-+            ad_group_ad.ad.responsive_display_ad.headlines, 
-+            ad_group_ad.ad.responsive_display_ad.long_headline, 
-+            ad_group_ad.ad.responsive_display_ad.descriptions, 
-
-RESPONSIVE_SEARCH_AD
-
-+           ad_group_ad.ad.responsive_search_ad.headlines, 
-+           ad_group_ad.ad.responsive_search_ad.descriptions, 
-+           ad_group_ad.ad.responsive_search_ad.path1, 
-+           ad_group_ad.ad.responsive_search_ad.path2, 
-
 VIDEO_RESPONSIVE_AD
-
             ad_group_ad.ad.video_responsive_ad.headlines, 
             ad_group_ad.ad.video_responsive_ad.descriptions, 
             ad_group_ad.ad.video_responsive_ad.long_headlines,
-
-TEXT_AD
-
-+            ad_group_ad.ad.text_ad.headline,
-+           ad_group_ad.ad.text_ad.description2,
-+           ad_group_ad.ad.text_ad.description1 
-
  */
